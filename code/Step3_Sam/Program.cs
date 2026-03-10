bf63c6d3df5e02170e33fe100b5c114b2145aa5e
s0m1k111 <spavel1207@gmail.com>
2026-03-10 12:45:42 +0300

    Step 3: conditional algorithm and if/else in C#

---
+++ code/Step3_Sam/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Выбор одежды на день");
+Console.WriteLine("Сегодня праздник?: ");
+string odejda = Console.ReadLine();
+
+if (odejda == "Yes")
+{
+    Console.WriteLine("Оденьте смокинг или платье");
+}
+else
+{
+    Console.WriteLine("Как обычно");
+}
+Console.WriteLine("WW");

bb0a2d775671a56002f86ddfc363f534a45a6743
s0m1k111 <spavel1207@gmail.com>
2026-03-16 15:51:06 +0300

    Step 7 arrays

---
+++ code/Step7_ArrayElementRunLab17/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Вывод элементов массива:");
+int[] numbers = { 101, 20, 30, 42, 52 };
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Элемент [{i}]: {numbers[i]}");
+}
---
+++ code/Step7_ArrayMaxRunLab17/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 5, 32, 11, 52, 3, 17 };
+int max = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+         max = array[i];
+    }
+}
+
+Console.WriteLine($"Массивчик как у богов: {string.Join(", ", array)}");
+Console.WriteLine($"Максимальный профессиональный элемент: {max}");
